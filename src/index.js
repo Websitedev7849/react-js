69a13fbd699e8f203be3d1a1461bf5c9710f4b67
sidd7849 <68861220+sidd7849@users.noreply.github.com>
2021-06-29 20:19:58 +0530

    deleted test file, performance check file, understood structure of react app, this is our won root commit, all branches will be created from this commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+/* 
+* the following code is added between main.chunk.js on npm run start ...
+* this js file is appended to index.html and then executed on browser 
+*/
+
+console.log("how is this been logged");
+console.log("this is another log and this log is main.chunk.js")
+
+const myOwnVariable = "my string";
+
+
+
+console.log(myOwnVariable)
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +23,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

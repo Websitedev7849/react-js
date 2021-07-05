e828f7d0c3432983505d89152f456fd244d2dfdd
sidd7849 <68861220+sidd7849@users.noreply.github.com>
2021-07-05 15:05:49 +0530

    imported custom component(Navbar) succesfully

--- src/index.js
+++ src/index.js
@@ -3,19 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-/* 
-* the following code is added between main.chunk.js on npm run start ...
-* this js file is appended to index.html and then executed on browser 
-*/
-
-console.log("how is this been logged");
-console.log("this is another log and this log is main.chunk.js")
-
-const myOwnVariable = "my string";
-
-
-
-console.log(myOwnVariable)
 
 ReactDOM.render(
   <React.StrictMode>

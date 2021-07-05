56d303bc9abb1a95034aaa6f2adaa4c2a23db942
sidd7849 <68861220+sidd7849@users.noreply.github.com>
2021-07-05 15:36:44 +0530

    added styles using index.css

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1>My Blogs</h1>
+            <h1 className="web-head">My Blogs</h1>
             <div className="links">
                 <a href = "/">Home</a>
                 <a href = "/add-blog">Add Blog</a>

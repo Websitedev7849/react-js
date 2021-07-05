24bedea04ede52024ef509046ba999104750cde0
sidd7849 <68861220+sidd7849@users.noreply.github.com>
2021-07-05 18:53:02 +0530

    trying to create packaged app with electron builder but not working

--- public/Main.js
+++ public/Main.js
@@ -1,6 +1,7 @@
+const path = require('path');
 const {app, BrowserWindow} = require('electron');
 
 app.on('ready', () => {
     let win = new BrowserWindow({width: 800, height: 600});
-    win.loadURL('http://localhost:3000/');
+    win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
 });

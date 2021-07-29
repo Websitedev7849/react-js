242a2ef7423cf2a872d058bb8ae8a9574ff53ba8
Siddharth Pawar <websitedev7849@gmail.com>
2021-07-29 16:08:29 +0530

    done with navbar responsiveness, having ISSUE with app height

--- src/Navbar.js
+++ src/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = ({fill, theme, useFill, setSearchWord}) => {
             
             <svg fill={fill} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px"><path fill="none" stroke={fill} strokeMiterlimit="10" strokeWidth="2" d="M13 4A9 9 0 1 0 13 22A9 9 0 1 0 13 4Z"/><path fill="none" stroke={fill} strokeLinecap="round" strokeMiterlimit="10" strokeWidth="2" d="M26 26L19.437 19.437"/></svg>
 
-            <input type="text" onChange={e => setSearchWord(e.target.value)} placeholder="Search by Text, Date..." className="search"/>
+            <input className="search" type="text" onChange={e => setSearchWord(e.target.value)} placeholder="Search by Text, Date..." />
         </div>
         <h1 className="Notes">Notes</h1>
      </nav>
